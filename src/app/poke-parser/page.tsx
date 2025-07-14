9c6738017df707a33f4f16041ca83385b1bc4c20
Kevin Cauto <kevinmcauto@gmail.com>
2025-07-14 08:57:47 -0400

    feat: Introduce new layout and styling for PokeParserPage and enhance PokeParserForm with improved design

--- src/app/poke-parser/page.tsx
+++ src/app/poke-parser/page.tsx
@@ -1,11 +1,13 @@
 import PokeParserForm from './PokeParserForm';
+import styles from './page.module.css';
 
 export const metadata = { title: 'Poké Parser | Kevin Cauto' };
 
 export default function PokeParserPage() {
   return (
-    <main>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Pokemon Single Log Parser</h1>
       <PokeParserForm />
-    </main>
+    </div>
   );
 }
